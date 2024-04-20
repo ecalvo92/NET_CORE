69b4a8bb01343a74571ac696fde2a052ce62bdda
ecalvo92 <edcalvo92@hotmail.com>
2024-04-19 19:20:03 -0600

    Versión inicial del proyecto Web API Net Core

---
+++ NetCore_API/NetCore_API/Controllers/UsuarioController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NetCore_API.Entities;
+
+namespace NetCore_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsuarioController : ControllerBase
+    {
+        public static List<Usuario> lista = new List<Usuario>();
+
+        [HttpGet]
+        [Route("ConsultarUsuarios")]
+        public IActionResult ConsultarUsuarios()
+        {
+            return Ok(lista);
+        }
+
+        [HttpGet]
+        [Route("ConsultarUsuario")]
+        public IActionResult ConsultarUsuario(int Id)
+        {
+            var resultado = lista.Where(x => x.Id == Id).FirstOrDefault();
+            return Ok(resultado);
+        }
+
+        [HttpPost]
+        [Route("AgregarUsuario")]
+        public IActionResult AgregarUsuario(Usuario Entidad)
+        {
+            lista.Add(Entidad);
+            return Ok(lista);
+        }
+
+        [HttpPut]
+        [Route("ActualizarUsuario")]
+        public IActionResult ActualizarUsuario(Usuario Entidad)
+        {
+            foreach (var item in lista.Where(x => x.Id == Entidad.Id))
+            {
+                item.Nombre = Entidad.Nombre;
+                item.Estado = Entidad.Estado;
+            }
+
+            return Ok(lista);
+        }
+
+        [HttpDelete]
+        [Route("EliminarUsuario")]
+        public IActionResult EliminarUsuario(int Id)
+        {
+            var resultado = lista.Where(x => x.Id == Id).FirstOrDefault();
+            lista.Remove(resultado!);
+            return Ok(lista);
+        }
+
+    }
+}
---
+++ NetCore_API/NetCore_API/Entities/Usuario.cs
@@ -0,0 +1,9 @@
+﻿namespace NetCore_API.Entities
+{
+    public class Usuario
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; } = string.Empty;
+        public bool Estado { get; set; }
+    }
+}
